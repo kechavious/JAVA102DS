c8cb66f5062599fa80a7fdaab520755803e42cad
kechavious <zkz758652@gmail.com>
2024-12-06 17:33:19 -0500

    Solution of the Network problem

--- src/lab13/Solution_Network.java
+++ src/lab13/Solution_Network.java
@@ -2,28 +2,30 @@ package lab13;
 
 public class Solution_Network {
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n]; // 标记每个节点是否被访问
-        int networkCount = 0;
+        // 用于记录每个节点是否访问过
+        boolean[] visited = new boolean[n];
+        int numNetworks = 0;
 
-        // 遍历每个节点
+        // 遍历所有节点
         for (int i = 0; i < n; i++) {
+            // 如果当前节点未访问，说明发现了一个新网络
             if (!visited[i]) {
+                numNetworks++;
                 dfs(computers, visited, i);
-                networkCount++; // 每次执行 DFS，表示发现一个新的网络
             }
         }
 
-        return networkCount;
+        return numNetworks;
     }
 
-    // 深度优先搜索递归方法
-    private void dfs(int[][] computers, boolean[] visited, int node) {
-        visited[node] = true; // 标记当前节点为已访问
+    // 深度优先搜索 (DFS)
+    private void dfs(int[][] isConnected, boolean[] visited, int node) {
+        visited[node] = true;
 
-        // 遍历与当前节点直接相连的其他节点
-        for (int i = 0; i < computers.length; i++) {
-            if (computers[node][i] == 1 && !visited[i]) {
-                dfs(computers, visited, i);
+        // 遍历与当前节点相连的所有节点
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visited[i]) {
+                dfs(isConnected, visited, i); // 递归访问相连节点
             }
         }
     }
